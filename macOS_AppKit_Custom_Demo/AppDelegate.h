4fe209b500acd43548ceed591376aae43fddc725
tangzhifeng <330200387@qq.com>
2019-11-21 09:37:44 +0800

    Initial Commit

---
+++ macOS_AppKit_Custom_Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  macOS_AppKit_Custom_Demo
+//
+//  Created by xia luzheng on 2019/11/21.
+//  Copyright © 2019 xia luzheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ macOS_AppKit_Custom_Demo/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  macOS_AppKit_Custom_Demo
+//
+//  Created by xia luzheng on 2019/11/21.
+//  Copyright © 2019 xia luzheng. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
